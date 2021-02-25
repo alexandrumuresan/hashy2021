4a08ba3eb717927c4b957a11cb6af25794e2ab83
Gyarmathy Timea <gyarmathy.timi@hotmail.com>
2021-02-25 20:29:27 +0100

    Simple logic and output writing

--- io_utils.py
+++ io_utils.py
@@ -6,7 +6,7 @@ def read_input(input_file):
     print("reading input")
     file = open(input_file, 'r')
 
-    d, i, s, v, f = file.readline().rstrip("\n").rstrip("\r").split(' ')
+    d, intersection, s, v, f = file.readline().rstrip("\n").rstrip("\r").split(' ')
 
     DG = nx.DiGraph()
 
@@ -16,8 +16,8 @@ def read_input(input_file):
         DG.add_weighted_edges_from([(int(b), int(e), int(l))])
         DG[int(b)][int(e)]['label'] = name
 
-    nx.draw(DG, with_labels=True, font_weight='bold')
-    plt.show()
+    # nx.draw(DG, with_labels=True, font_weight='bold')
+    # plt.show()
 
     cars = []
     for i in range(int(v)):
@@ -27,13 +27,28 @@ def read_input(input_file):
 
     file.close()
 
-    return d, i, s, v, f, DG, cars
+    return d, intersection, s, v, f, DG, cars
 
 
 def write_output(output_file, content):
     print("writing output")
     f = open(output_file, "w")
 
-    f.write(content)
+    nr_lights = len(content)
+
+    f.write(str(nr_lights))
+    f.write("\n")
+
+    for i in range(nr_lights):
+        f.write(str(content[i]['intersection_index']))
+        f.write("\n")
+
+        f.write(str(content[i]['nr_streets']))
+        f.write("\n")
+
+        nr_street_lights = len(content[i]['street_data'])
+        for j in range(nr_street_lights):
+            f.write(content[i]['street_data'][j][0] + ' ' + str(content[i]['street_data'][j][1]))
+            f.write("\n")
 
     f.close()
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from io_utils import read_input
+from io_utils import read_input, write_output
+
+INPUT_CHAR = 'f'
 
 
 def main():
-    d, i, s, v, f, DG, cars = read_input('a.txt')
-    # print(DG[0][1]['weight'])
+    duration, intersection, streets, nr_cars, bonus, DG, car_routes = read_input(INPUT_CHAR + '.txt')
+
+    traffic = []
+    for i in range(int(intersection)):
+        if DG.in_degree(i) == 1:
+            street_name = list(DG.in_edges(i, data=True))[0][2]['label']
+            traffic.append({
+                'intersection_index': i,
+                'nr_streets': 1,
+                'street_data': [(street_name, 1)]
+            })
+
+    write_output(INPUT_CHAR + '_out.txt', traffic)
 
 
 if __name__ == "__main__":
